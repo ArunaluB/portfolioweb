e634a8dde294c05fd1376022221b2067ea15035e
ArunaluB <IT22515612@my.sliit.lk>
2023-12-02 19:44:58 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/my-work-app/",
+  base: "/my-work-app",
   plugins: [react()],
   server: {
     port: 8000
